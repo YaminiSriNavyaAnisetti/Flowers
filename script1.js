3b554d515f2c3f7eeb6bfe7781973b9d13c8113a
YaminiSriNavyaAnisetti <137883714+YaminiSriNavyaAnisetti@users.noreply.github.com>
2023-07-19 10:45:13 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,12 @@
+function rose()
+{
+   document.getElementById("project").src="https://www.ikea.com/in/en/images/products/smycka-artificial-flower-rose-red__0903311_pe596728_s5.jpg?f=s";
+}  
+function jasmini()
+{
+    document.getElementById("project").src="https://image.tcmwiki.com/image/%E8%8C%89%E8%8E%89%E8%8A%B1/%E8%8C%89%E8%8E%89%E8%8A%B1.jpg";
+}
+ function hibiscus()
+ {
+    document.getElementById("project").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3jXaU29HnaP5cong3XbDDFMZVdARwHabU-QvVCjXSV5kv0laAtrPgDpq_M2kM8XLwBpY&usqp=CAU";
+ }   
